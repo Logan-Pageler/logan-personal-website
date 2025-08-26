c58f8f9e795d0ecbb2a680905a9cfc7a0c16ddb1
Logan Pageler <pagelerlogan@gmail.com>
2025-08-25 23:43:06 -0700

    feat(homepage): Add a mission statement

--- src/app/homepage/homepage.ts
+++ src/app/homepage/homepage.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Slogan } from "./slogan/slogan";
+import { MissionStatement } from './mission-statement/mission-statement';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-homepage',
-  imports: [Slogan, CommonModule],
+  imports: [Slogan, CommonModule, MissionStatement],
   templateUrl: './homepage.html',
   styleUrl: './homepage.scss'
 })
---
+++ src/app/homepage/mission-statement/mission-statement.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mission-statement',
+  templateUrl: './mission-statement.html',
+  styleUrls: ['./mission-statement.scss']
+})
+export class MissionStatement {
+}
+
