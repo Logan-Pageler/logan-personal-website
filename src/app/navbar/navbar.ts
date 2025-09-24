1aa3ab5b8cef1e826a89ee47dd616c847567fde9
Logan Pageler <pagelerlogan@gmail.com>
2025-09-23 22:22:55 -0700

    feat(navbar): Squash links into hamburger button when screen to small

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,4 +1,6 @@
-import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter, Subscription } from 'rxjs';
 
 /**
  * Component to represent the navbar at the top of the screen.
@@ -15,6 +17,46 @@ export class Navbar implements OnInit{
   @Input() animated: boolean = false;
   // Whether the screen is scrolled
   scrolled = false;
+  // Whether the menu is open
+  isOpen = false;
+  // Subscription to router events
+  private sub: Subscription;
+
+  // Links to display in the navbar
+  links = [
+    { label: 'Home', path: '' },
+    { label: 'Projects', path: 'projects' },
+    { label: 'Contact', path: 'contact' },
+  ];
+
+  /**
+   * Connect to the router to listen for navigation events
+   * @param router The router to listen for navigation events
+   */
+  constructor(router: Router) {
+    // Close the menu after navigation
+    this.sub = router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe(() => (this.isOpen = false));
+  }
+
+  /**
+   * Toggle the menu open or closed
+   */
+  toggleMenu() { this.isOpen = !this.isOpen; }
+
+  /**
+   * Close the menu
+   */
+  closeMenu()  { this.isOpen = false; }
+
+  /**
+   * Listen for escape key to close the menu
+   */
+  @HostListener('window:keydown', ['$event'])
+  onKeydown(e: KeyboardEvent) {
+    if (e.key === 'Escape' && this.isOpen) this.closeMenu();
+  }
 
   /**
    * Setup the scrolled on init
@@ -30,4 +72,9 @@ export class Navbar implements OnInit{
   onWindowScroll() {
     this.scrolled = !this.animated || window.scrollY > 50;
   }
+
+  /**
+   * Cleanup the subscription
+   */
+  ngOnDestroy() { this.sub?.unsubscribe(); }
 }
